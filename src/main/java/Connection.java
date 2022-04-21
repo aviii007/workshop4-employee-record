baf516ab4c56d04e23c420518464da2a00597779
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-21 16:51:43 +0530

    Added to pojo class.

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -2,6 +2,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connection {
+    private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String DB_CONNECTION = "jdbc:oracle:thin:@localhost:1521:YourDatabase";
+    private static final String DB_USER = "user";
+    private static final String DB_PASSWORD = "password";
     public static void main(String[] args) {
         System.out.println("welcome to the workshop 4");
         String dbURL = "jdbc:mysql://localhost:3306/employee";
@@ -14,6 +18,7 @@ public class Connection {
 
             if (conn != null) {
                 System.out.println("Connected");
+
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
