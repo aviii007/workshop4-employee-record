665a5d588f874259e88a029e76dbe6fdc636f4f0
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-21 09:39:09 +0530

    Added the dependencies.

---
+++ src/main/java/Employee.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("welcome to the workshop 4");
+        String jdbcURL ="jdbc:mysql://localhost3306/employee?useSSL=false";
+        String userName="root";
+        String password="mysql123";
+        Connection connection;
+
+
+    }
+}
